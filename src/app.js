83a4c11f8c45e0a8c842b5c3bc42bb9f4fed0c17
Lizu-4 <lizugas2002@gmail.com>
2022-11-28 06:01:35 +0000

    lo basico ready

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+import "./index.html";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(iconPicker());
+  document.querySelector(".number").innerHTML = numberPicker();
+  console.log(numberPicker());
 };
+
+function numberPicker() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  return `${numbers[Math.floor(Math.random() * numbers.length)]}`;
+}
+
+function iconPicker() {
+  let icons = ["heart", "spade", "diamond", "club"];
+  return `${icons[Math.floor(Math.random() * icons.length)]}`;
+}
